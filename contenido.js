2c0085eda5727d64289f316609fc87a915fb94ba
Miranda-124 <A100237124@my.uvm.edu.mx>
2025-05-28 13:52:16 -0600

    Update contenido.js

--- contenido.js
+++ contenido.js
@@ -1,7 +1,3 @@
-// JS para futuras funcionalidades interactivas
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Contenido cargado: Leyes sostenibles para la igualdad de género");
-
-    // Ejemplo de interacción futura
-    // alert("Bienvenido a una visión justa y sostenible del mundo.");
-});
+function mostrarMensaje() {
+    alert("¡Gracias por tu interés! Te contactaremos con los próximos talleres disponibles.");
+}
